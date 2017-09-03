976163e32b5b3631cb8c5dca7ac65bfb9504c494
misspia <pia_leung@hotmail.com>
2017-09-03 01:24:00 -0400

    evo pid example

--- server/routes/evolutions.router.js
+++ server/routes/evolutions.router.js
@@ -3,6 +3,7 @@ const Evolutions = require('../controllers/evolutions.controller.js');
 
 // Examples
 // http://localhost:3001/api/v1/evolutions/all
+// http://localhost:3001/api/v1/evolutions/pid/n1
 
 router.get('/all', Evolutions.all); 
 router.get('/pid/:pid', Evolutions.pid);
