1896959e5d4a16053a62a7f98d3a94614cdb1722
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-09-03 23:49:36 -0700

    I'm sorry it's even more messy; it works though -- Evolutions fully working per spec

--- server/models/evolutions.model.js
+++ server/models/evolutions.model.js
@@ -17,14 +17,16 @@ const memberInfo = {
 			.catch((err) => { console.log('ERROR D: ', err) });
 	},
 	format: (res, members) => {
-		return members.map((member, index) => {
-			return {
-				[member]: {
-					main: memberInfo.getMain(res.main, member),
-					types: memberInfo.getTypes(res.types, member)
-				}
+		// I feel like 
+		let flattened = {};
+		members.map((member, index) => {
+			flattened[member] = {
+				main: memberInfo.getMain(res.main, member),
+				types: memberInfo.getTypes(res.types, member)
 			}
 		});
+
+		return flattened;
 	},
 	getMain: (rows, id) => {
 		let main;
@@ -76,72 +78,62 @@ const qEvo = {
 	format: (evoData) => {
 		const evoList = evoData.evolutions;
 		let evoObject = { evolutions: { base: {}, next: [] } };
-
-		// testing using http://localhost:3001/api/v1/evolutions/pid/n265
-		// the data is wrong though, it's missing, for example, base n268, n269
-		// http://localhost:3001/api/v1/evolutions/pid/n266
-		// should give same result as n265, but it doesn't
-		// also, look at Eevee's
-		// testing using http://localhost:3001/api/v1/evolutions/pid/n133
-
-		// basically I think what needs to happen is we want qEvo.stages to return
-		// the result of select * from pokedex.evolutions where stage0='n133';
-		// I think this should work 
-		// can you try it? gonna sleep now, but I believe that based on how I
-		// wrote this formatting function, just returning the result of that
-		// query should suffice
-
-		// TODO
-		// 1) ^ that
-		// 2) add 'main' information for each evolution
-		// 3) add 'type' information for each evolution
-
-		// stage0 processing
+		let memberList = [];
 		for(let i = 0; i < evoList.length; i++) {
-			if(evoList[i].base === evoList[i].stage0) {
-				if(evoObject.evolutions['base'].unique_id == undefined) {
-					evoObject.evolutions['base'].unique_id = evoList[i].stage0;
-					evoObject.evolutions['base'].condition = evoList[i].condition;
-				}
-
-				// stage1 processing
-				if(evoList[i].stage1 != null) {
-					for(let j = 0; j < evoList.length; j++) {
-						if(evoList[j].base == evoList[i].stage1) {
-							console.log(evoList[j].base)
-							// Checks if the next evolution exists already to prevent dupes
-							if(evoObject.evolutions.next.filter(nextEvo => nextEvo.unique_id == evoList[j].base).length == 0) {
-								evoObject.evolutions.next.push({
-									unique_id: evoList[j].base,
-									condition: evoList[j].condition,
-									next: []
-								});
-							} 
-							//stage 2 processing 
-							if(evoList[i].stage2 != null) { 
-								for(let k = 0; k < evoList.length; k++) {
-									console.log(evoList[k].base , 'base')
-									console.log(evoList[i].stage2 , 'stage2')
-									console.log(evoList[k].base == evoList[i].stage2)
-									console.log('==========================================')
-									if(evoList[k].base == evoList[i].stage2) {
-										console.log(evoList[k].base)
-										const currentEvoIndex = evoObject.evolutions.next.length - 1;
-										if(evoObject.evolutions.next[currentEvoIndex].next.filter(nextEvo => nextEvo.unique_id == evoList[k].base).length == 0) {
-											evoObject.evolutions.next[currentEvoIndex].next.push({
-												unique_id: evoList[k].base,
-												condition: evoList[k].condition
-											});
-										} 
-									}
-								}
-							}
-						}
-					}	
-				}
+			if(memberList.indexOf(evoList[i].base) == -1) {
+				memberList.push(evoList[i].base);
 			}
 		}
-		return evoObject;
+
+		return memberInfo.get(memberList)
+			.then((pokemonInfo) => { 
+				// stage0 processing
+				for(let i = 0; i < evoList.length; i++) {
+					if(evoList[i].base === evoList[i].stage0) {
+						if(evoObject.evolutions['base'].unique_id == undefined) {
+							evoObject.evolutions['base'].unique_id = evoList[i].stage0;
+							evoObject.evolutions['base'].condition = evoList[i].condition;
+							Object.assign(evoObject.evolutions['base'], pokemonInfo[evoList[i].base]);
+						}
+
+						// stage1 processing
+						if(evoList[i].stage1 != null) {
+							for(let j = 0; j < evoList.length; j++) {
+								if(evoList[j].base == evoList[i].stage1) {
+									// Checks if the next evolution exists already to prevent dupes
+									if(evoObject.evolutions.next.filter(nextEvo => nextEvo.unique_id == evoList[j].base).length == 0) {
+										let nextStageInfo = {
+											unique_id: evoList[j].base,
+											condition: evoList[j].condition
+										}
+										Object.assign(nextStageInfo, pokemonInfo[evoList[j].base], { next: [] })
+										evoObject.evolutions.next.push(nextStageInfo);
+									} 
+									//stage 2 processing 
+									if(evoList[i].stage2 != null) { 
+										for(let k = 0; k < evoList.length; k++) {
+											if(evoList[k].base == evoList[i].stage2) {
+												console.log(evoList[k].base)
+												const currentEvoIndex = evoObject.evolutions.next.length - 1;
+												if(evoObject.evolutions.next[currentEvoIndex].next.filter(nextEvo => nextEvo.unique_id == evoList[k].base).length == 0) {
+													let nextStageInfo = {
+														unique_id: evoList[k].base,
+														condition: evoList[k].condition
+													}
+													Object.assign(nextStageInfo, pokemonInfo[evoList[k].base], { next: [] })
+													evoObject.evolutions.next[currentEvoIndex].next.push(nextStageInfo);
+												} 
+											}
+										}
+									}
+								}
+							}	
+						}
+					}
+				}
+				return evoObject;
+			})
+			.catch((err) => { return err; });		
 	}
 };
 
