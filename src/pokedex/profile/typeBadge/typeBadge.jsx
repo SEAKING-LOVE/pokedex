8ebbc0245c5ec3b7a7a95a3999d6a461ffd8cd28
misspia <pia_leung@hotmail.com>
2017-09-22 19:36:10 -0400

    type badge tooltip

--- src/pokedex/profile/typeBadge/typeBadge.jsx
+++ src/pokedex/profile/typeBadge/typeBadge.jsx
@@ -7,10 +7,13 @@ class SpriteCard extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			type: this.props.type.toLowerCase()
+			type: this.props.type.toLowerCase(),
+			tooltip: false
 		}
+		this.revealTooltip = this.revealTooltip.bind(this);
+		this.hideTooltip = this.hideTooltip.bind(this);
 	}
-	setStyle() {
+	typeBadgeStyle() {
 		const color = typeColors[this.state.type]();
 		return {
 			backgroundColor: color
@@ -21,11 +24,29 @@ class SpriteCard extends Component {
 		const type = this.state.type;
 		return require(`./symbols/${type}.png`);
 	}
+	revealTooltip() {
+		this.setState({
+			tooltip: true
+		});
+	}
+	hideTooltip() {
+		this.setState({
+			tooltip: false
+		});
+	}
+	tooltipStyle() {
+		const color = typeColors[this.state.type]();
+		return {
+			display: this.state.tooltip ? 'flex' : 'none',
+			backgroundColor: color
+		}
+	}
 	render() {
-		return <div className="typeBadge" style={this.setStyle()}>
-			<img
-			src={this.requireSymbol()}
-			alt=""/>
+		return <div className="typeBadge" style={this.typeBadgeStyle()}
+			onMouseOver={this.revealTooltip}
+			onMouseOut={this.hideTooltip}>
+			<div className='tooltip' style={this.tooltipStyle()}>{this.props.type}</div>
+			<img src={this.requireSymbol()} alt={`${this.props.type} type`}/>
 		</div>
 	}
 }
